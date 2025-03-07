d3183b90209c322f9b01a9ca292879f3d374c70f
OmoladeTrust <ladeakingboye@gmail.com>
2025-03-07 18:39:46 +0100

    Initialize project using Create React App

---
+++ hashmap/twosums.js
@@ -0,0 +1,9 @@
+function twoSums (numbers, target) {
+  const hashMap = {}
+  for (let i=0; i<numbers.length; i++){
+    const remainingSum = target - numbers[i];
+    if(remainingSum in hashMap) {
+      ret
+    }
+  }
+}
